35a4171d34d7f41791c8dfdc8255db67b225efe5
Ruan <theruangamer@gmail.com>
2026-09-02 13:55:57 -0300

    feat: adicionar fila de atendimento

--- src/main/java/pmesp/helpdesk37bpmm/Chamado/ChamadoRepository.java
+++ src/main/java/pmesp/helpdesk37bpmm/Chamado/ChamadoRepository.java
@@ -2,11 +2,13 @@ package pmesp.helpdesk37bpmm.Chamado;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pmesp.helpdesk37bpmm.Usuario.UsuarioModel;
-
 import java.util.List;
 
 public interface ChamadoRepository extends JpaRepository<ChamadoModel, Long> {
 
     // Buscar chamados abertos de um usuário
     List<ChamadoModel> findBySolicitanteAndStatus(UsuarioModel solicitante, ChamadoStatus status);
+
+    // Buscar chamados de um status do mais antigo para o mais recente
+    List<ChamadoModel> findByStatusOrderByDataAberturaAsc(ChamadoStatus status);
 }
